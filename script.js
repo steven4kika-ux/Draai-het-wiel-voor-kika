0f60acfbd45c1bcba1beded73f871750b1ad3be0
steven4kika-ux <steven4kika@gmail.com>
2026-01-21 14:56:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const colors = [
 const angle = (Math.PI * 2) / colors.length;
 
 function drawWheel() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   colors.forEach((color, i) => {
     ctx.beginPath();
@@ -40,3 +39,36 @@ function drawWheel() {
 }
 
 drawWheel();
+
+let rotation = 0;
+let spinning = false;
+
+function spinWheel() {
+  if (spinning) return;
+  spinning = true;
+
+  const spinTime = 2000; // 2 seconden
+  const start = performance.now();
+  const extraRotation = Math.random() * Math.PI * 8 + Math.PI * 6;
+
+  function animate(time) {
+    const progress = Math.min((time - start) / spinTime, 1);
+    rotation = extraRotation * progress;
+
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.translate(cx, cy);
+    ctx.rotate(rotation);
+    ctx.translate(-cx, -cy);
+
+    drawWheel();
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      spinning = false;
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
